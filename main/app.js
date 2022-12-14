f8cd8b85d4268eafbf8fe337b75d2254e1725b5c
mateicarina <115937167+mateicarina@users.noreply.github.com>
2022-12-14 23:41:08 +0200

    Add files via upload

---
+++ main/app.js
@@ -0,0 +1,102 @@
+const express = require('express')
+const Sequelize = require('sequelize')
+
+const sequelize = new Sequelize({
+    dialect: 'sqlite',
+    storage: 'my.db'
+});
+
+
+
+sequelize.authenticate().then(() => {
+    console.log('Connection has been established successfully.');
+}).catch((error) => {
+    console.error('Unable to connect to the database: ', error);
+});
+
+let FoodItem = sequelize.define('foodItem', {
+    name: Sequelize.STRING,
+    category: {
+        type: Sequelize.STRING,
+        validate: {
+            len: [3, 10]
+        },
+        allowNull: false
+    },
+    calories: Sequelize.INTEGER
+}, {
+    timestamps: false
+})
+
+
+const app = express()
+app.use(express.json());
+
+
+
+app.get('/create', async (req, res) => {
+    try {
+        await sequelize.sync({ force: true })
+        for (let i = 0; i < 10; i++) {
+            let foodItem = new FoodItem({
+                name: 'name ' + i,
+                category: ['MEAT', 'DAIRY', 'VEGETABLE'][Math.floor(Math.random() * 3)],
+                calories: 30 + i
+            })
+            await foodItem.save()
+        }
+        res.status(201).json({ message: 'created' })
+    }
+    catch (err) {
+        console.warn(err.stack)
+        res.status(500).json({ message: 'server error' })
+    }
+})
+
+app.get('/food-items', async (req, res) => {
+    try {
+        let foodItems = await FoodItem.findAll()
+        res.status(200).json(foodItems)
+    }
+    catch (err) {
+        console.warn(err.stack)
+        res.status(500).json({ message: 'server error' })
+    }
+})
+
+app.post('/food-items', async (req, res) => {
+    let { name, category, calories } = req.body;
+
+    try {
+        const body = req.body;
+        if (Object.keys(body).length === 0) {
+            res.status(400).json({ "message": "body is missing" });
+        } else {
+
+            if (Object.keys(body).length < 3) {
+                res.status(400).json({ "message": "malformed request" });
+            }
+
+            if (body.calories < 0) {
+                res.status(400).json({ "message": "calories should be a positive number" });
+            }
+
+
+            if (!['MEAT', 'DAIRY', 'VEGETABLE'].includes(body.category))
+                res.status(400).json({ "message": "not a valid category" });
+
+            const newFoodItem = await FoodItem.create(req.body);
+            res.status(201).json({ "message": "created" });
+
+        }
+
+
+
+    }
+    catch (err) {
+        console.warn(err);
+        //res.status(500).json({ "message": "server error" })
+    }
+})
+
+module.exports = app
